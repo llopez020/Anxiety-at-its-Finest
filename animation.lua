735baa81c73af2d932ef13f63e2189dd30f7ce2f
vmikelic <vmikelic@nmsu.edu>
2023-03-30 18:36:47 -0600

    added animation flipping

--- animation.lua
+++ animation.lua
@@ -7,7 +7,8 @@ animation = {
     sprite_width = 0,
     sprite_height = 0,
     frame_cycle = 1,
-    current_frame = 1
+    current_frame = 1,
+    flipped = false
 }
 
 function animation:new(pos_x_,pos_y_,sprite_indices_,sprite_width_,sprite_height_,frame_cycle_)
@@ -21,12 +22,15 @@ function animation:new(pos_x_,pos_y_,sprite_indices_,sprite_width_,sprite_height
     self)
 end
 
-function animate(object,pos_x,pos_y,sprite_indices,sprite_width,sprite_height,frame_cycle)
+function animate(object,pos_x,pos_y,sprite_indices,sprite_width,sprite_height,frame_cycle,flipped)
     if (object==nil) then
         return
     end
     if (animation_data[obj_hash(object)]==nil) then
         a = animation:new(pos_x,pos_y,sprite_indices,sprite_width,sprite_height,frame_cycle)
+        if (flipped!=nil) then
+            a.flipped = true
+        end
         animation_data[obj_hash(object)] = a
     end
     animation_data[obj_hash(object)].pos_x = pos_x
@@ -34,8 +38,11 @@ function animate(object,pos_x,pos_y,sprite_indices,sprite_width,sprite_height,fr
     play_animation(animation_data[obj_hash(object)])
 end
 
-function animate_once(pos_x,pos_y,sprite_indices,sprite_width,sprite_height,frame_cycle,num_cycles)
+function animate_once(pos_x,pos_y,sprite_indices,sprite_width,sprite_height,frame_cycle,num_cycles,flipped)
     a = animation:new(pos_x,pos_y,sprite_indices,sprite_width,sprite_height,frame_cycle)
+    if (flipped!=nil) then
+        a.flipped = true
+    end
     add(animation_data_once,{anim=a,num_cycles=(#a.sprite_indices*num_cycles)})
 end
 
@@ -58,7 +65,7 @@ function play_animation(animation)
         animation.current_frame = 0
         spr_select = 0
     end
-    spr(animation.sprite_indices[spr_select+1], animation.pos_x, animation.pos_y, animation.sprite_width, animation.sprite_height)
+    spr(animation.sprite_indices[spr_select+1], animation.pos_x, animation.pos_y, animation.sprite_width, animation.sprite_height, animation.flipped)
     animation.current_frame = animation.current_frame+1
 end
 
