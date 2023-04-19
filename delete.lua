c91674afc5dac8c969f3f50333bb33716a12e040
llopez020 <89426021+llopez020@users.noreply.github.com>
2023-04-19 17:52:26 -0400

    Add files via upload

--- delete.lua
+++ delete.lua
@@ -1,10 +1,20 @@
 function deleteobj(a)
     local temp = objlist
-    local temp2 = nil
+    local temp2 = objlist
     while temp do
      if temp.value==a then temp2.next=temp.next goto leave end
      temp2 = temp
         temp = temp.next
     end
     ::leave::
+end
+
+function deleteallof(a, range)
+    local temp = objlist
+    local temp2 = nil
+    while temp do
+     if temp.value.id==a and (abs(temp.value.x-range))<100 then temp2.next=temp.next end
+     temp2 = temp
+        temp = temp.next
+    end
 end
